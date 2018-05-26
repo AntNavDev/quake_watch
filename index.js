c2c4c2bcd2a790951ef683afbc98f1d874405fdf
Anthony <anthonarro@gmail.com>
2018-05-25 20:01:18 -0700

    Remove test code and format

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ $(document).ready(function(){
     var regionIndicator;
     var markerCluster;
 
-
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
         zoom: 13
@@ -52,20 +51,11 @@ $(document).ready(function(){
     var input = document.getElementById('pac-input');
     var types = document.getElementById('type-selector');
 
-    //map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
     var autocomplete = new google.maps.places.Autocomplete(input);
-
-    //autocomplete.bindTo('bounds', map);
 
     var infowindow = new google.maps.InfoWindow();
     var infowindowContent = document.getElementById('infowindow-content');
     infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-      map: map,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
-
 
     autocomplete.addListener('place_changed', function(){
         var place = autocomplete.getPlace();
@@ -86,7 +76,6 @@ $(document).ready(function(){
 
         infowindowContent.children['place-name'].textContent = place.name;
         infowindowContent.children['place-address'].textContent = address;
-        
 
     });
 
